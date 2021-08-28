3ffb7ac7abc90b7c0d6072fe2e5e12c3d4f19f36
LuizaVanelli <luizavanelli.vanelli@gmail.com>
2021-08-27 21:19:23 -0300

    att EtecAB

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EtecAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ForegroundColor = ConsoleColor.Cyan;
+           Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
